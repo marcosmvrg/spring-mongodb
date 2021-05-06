e64e3d5f8ed73d273bc9d7a8e068ae766bf34412
Marcos Vinicius Rocha Gomes <marcosmvrg@gmail.com>
2021-05-06 10:54:20 +0100

    dbref
    criando dbref

---
+++ src/main/java/com/curso/mongodb/config/Instantiation.java
@@ -0,0 +1,52 @@
+package com.curso.mongodb.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.curso.mongodb.domain.Post;
+import com.curso.mongodb.domain.User;
+import com.curso.mongodb.dto.AuthorDTO;
+import com.curso.mongodb.repository.PostRepository;
+import com.curso.mongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Autowired
+	private PostRepository postReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));		
+		
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
+	}
+
+
+
+}
--- src/main/java/com/curso/mongodb/domain/User.java
+++ src/main/java/com/curso/mongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.curso.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	
 	@Field(name = "email")
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<Post>();
 
 	public User() {
 		super();
@@ -62,6 +68,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
 	}
 
 	@Override
