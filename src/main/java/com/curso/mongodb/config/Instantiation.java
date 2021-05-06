4fed38ca0f4fa7634382f7b2877949365cbeb090
Marcos Vinicius Rocha Gomes <marcosmvrg@gmail.com>
2021-05-06 11:07:44 +0100

    comments
    adicionando comentarios no post

--- src/main/java/com/curso/mongodb/config/Instantiation.java
+++ src/main/java/com/curso/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.curso.mongodb.domain.Post;
 import com.curso.mongodb.domain.User;
 import com.curso.mongodb.dto.AuthorDTO;
+import com.curso.mongodb.dto.CommentDTO;
 import com.curso.mongodb.repository.PostRepository;
 import com.curso.mongodb.repository.UserRepository;
 
@@ -39,7 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));		
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/curso/mongodb/domain/Post.java
+++ src/main/java/com/curso/mongodb/domain/Post.java
@@ -1,12 +1,15 @@
 package com.curso.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.curso.mongodb.dto.AuthorDTO;
+import com.curso.mongodb.dto.CommentDTO;
 
 @Document(collation = "post")
 public class Post implements Serializable {
@@ -19,6 +22,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<CommentDTO>();
 
 	public Post() {
 		super();
@@ -79,6 +83,14 @@ public class Post implements Serializable {
 
 	public AuthorDTO getAuthor() {
 		return author;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
